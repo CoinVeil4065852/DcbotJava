aaf1d3d21362b2af35a3493ccd73f171ee7c6380
CoinVeil4065852 <CoinVeil4065852@users.noreply.github.com>
2020-10-25 22:15:01 +0800

    ww

--- src/main/java/com/coin/discordBot/Main.java
+++ src/main/java/com/coin/discordBot/Main.java
@@ -1,5 +1,6 @@
 package com.coin.discordBot;
 
+
 import com.coin.discordBot.events.Events;
 import com.coin.discordBot.events.features.Arithmetic;
 import com.coin.discordBot.events.features.Nim;
@@ -17,23 +18,6 @@ public class Main {
     public static JDA jda;
     public static void main (String[] agrs) throws Exception {
         Save.readAll();
-
-/*        JPanel jPanel = new JPanel();
-        JFrame jFrame = new JFrame();
-
-        jFrame.setSize(800,600);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
-        jFrame.add(jPanel);
-        jFrame.setTitle("Coin's DCBot");
-
-        jPanel.setLayout(null);
-
-        JLabel jLabel = new JLabel();
-        jLabel.setBounds(10,20,80,25);
-        jPanel.add(jLabel);*/
-
-
         jda = new JDABuilder(AccountType.BOT).setToken("NzQxOTE1NjM0OTA4MjY2NTI2.Xy-gxw.4cPdIA8OZyGaeh-6Cnr9ODkzJdI").setActivity(Activity.playing("Cooking Lo Mei......")).build();
         jda.addEventListener(new Events());
         jda.addEventListener(new Arithmetic());
